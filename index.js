2b8c096202bca18fecbc4df440f324fe04da440f
ringoXD <nep360i@gmail.com>
2024-11-05 08:08:30 +0000

    clearAll

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 //<img src="./assets/cards/ace_of_hearts.png" height="128" draggable="false">
 
+async function ClearPlayerCards() {
+    const elem = document.getElementById('player_cards')
+    Array.from(elem.children).forEach((el) => {
+        el.remove()
+    });
+}
+
+async function ClearDealerCards() {
+    const elem = document.getElementById('dealer_cards')
+    Array.from(elem.children).forEach((el) => {
+        el.remove()
+    });
+}
+
+async function ClearAllCards() {
+    await ClearDealerCards()
+    await ClearPlayerCards()
+}
+
 async function AddCards(card) {
-    const elem = document.createElement('img')
-    elem.src = `./assets/cards/${card}.png`
-    elem.height = 128
-    elem.draggable = false
+    const img = document.createElement('img')
+    img.src = `./assets/cards/${card}.png`
+    img.height = 128
+    img.draggable = false
 }
 
 async function main() {
