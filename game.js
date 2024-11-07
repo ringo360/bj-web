cc4ad74d5c458cee0b17b073f23a7f0ba6cce38c
ringoXD <nep360i@gmail.com>
2024-11-07 08:08:51 +0000

    progress

--- game.js
+++ game.js
@@ -1,7 +1,44 @@
 const $g = (id => {return document.getElementById(id)})
 
-$g('panel_hit').onclick = handleClick
-$g('panel_stand').onclick = handleClick
-$g('panel_bet').onclick = handleClick
+$g('panel_hit').onclick = game_HIT
+$g('panel_stand').onclick = game_STAND
+$g('panel_bet').onclick = game_BET
 
-console.log('[EventListener] Called')
+async function ClearPlayerCards() {
+    const elem = $g('player_cards')
+    Array.from(elem.children).forEach((el) => {
+        el.remove()
+    });
+}
+
+async function ClearDealerCards() {
+    const elem = $g('dealer_cards')
+    Array.from(elem.children).forEach((el) => {
+        el.remove()
+    });
+}
+
+async function ClearAllCards() {
+    await ClearDealerCards()
+    await ClearPlayerCards()
+}
+
+async function AddCards(card, p) {
+    const img = document.createElement('img')
+    img.src = `./assets/cards/${card}.png`
+    img.height = 128
+    img.draggable = false
+}
+
+async function game_HIT() {
+    console.log('uwu')
+}
+async function game_STAND() {
+    console.log('uwu')
+}
+async function game_BET() {
+    console.log('uwu')
+}
+
+
+console.log('[Game] Called')
--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 //<img src="./assets/cards/ace_of_hearts.png" height="128" draggable="false">
 
-async function ClearPlayerCards() {
-    const elem = document.getElementById('player_cards')
-    Array.from(elem.children).forEach((el) => {
-        el.remove()
-    });
-}
-
-async function ClearDealerCards() {
-    const elem = document.getElementById('dealer_cards')
-    Array.from(elem.children).forEach((el) => {
-        el.remove()
-    });
-}
-
-async function ClearAllCards() {
-    await ClearDealerCards()
-    await ClearPlayerCards()
-}
-
-async function AddCards(card) {
-    const img = document.createElement('img')
-    img.src = `./assets/cards/${card}.png`
-    img.height = 128
-    img.draggable = false
-}
-
 async function main() {
     const loader = await import('./loader.js')
     loader.Call()
